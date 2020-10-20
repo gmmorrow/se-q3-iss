325e44df34637ee379d8658085e45eeea6db3620
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2020-10-20 12:14:16 -0400

    part A complete

--- iss.py
+++ iss.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+# __author__ = "Gabrielle"
 
 
-def main():
-    pass
+import requests
+import time
+import turtle
+import json
 
+response = requests.get("http://api.open-notify.org/astros.json")
 
-if __name__ == '__main__':
-    main()
+def astronaut_list(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+astronaut_list(response.json())
+
+# def main():
+#     astronaut_list()
+
+# if __name__ == '__main__':
+#     main()
