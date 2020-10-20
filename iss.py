aa0b993127c4a6bce3f487dc4eb49e5c3962b15c
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2020-10-20 12:45:31 -0400

    part B complete

--- iss.py
+++ iss.py
@@ -8,13 +8,21 @@ import time
 import turtle
 import json
 
-response = requests.get("http://api.open-notify.org/astros.json")
+astronauts = requests.get("http://api.open-notify.org/astros.json")
 
 def astronaut_list(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-astronaut_list(response.json())
+astronaut_list(astronauts.json())
+
+parameters = {
+    "lat": -4.2196,
+    "lon": 170.6524
+}
+coordinates = requests.get("http://api.open-notify.org/iss-now.json", params=parameters)
+astronaut_list(coordinates.json())
+    
 
 # def main():
 #     astronaut_list()
