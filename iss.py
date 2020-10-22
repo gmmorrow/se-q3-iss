eb4296e1ff0b35ea45a04ed66811f1b2192cb213
gabrielle@Gabbys-MacPro.com <gab_morrow@yahoo.com>
2020-10-22 10:41:28 -0400

    part B complete

--- iss.py
+++ iss.py
@@ -1,31 +1,50 @@
 #!/usr/bin/env python
 
-# __author__ = "Gabrielle"
+__author__ = "Gabrielle"
 
 
-import requests
+import json
 import time
 import turtle
-import json
+import requests
+import urllib.request
+
 
 astronauts = requests.get("http://api.open-notify.org/astros.json")
+
 
 def astronaut_list(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-astronaut_list(astronauts.json())
 
-parameters = {
-    "lat": -4.2196,
-    "lon": 170.6524
-}
-coordinates = requests.get("http://api.open-notify.org/iss-now.json", params=parameters)
-astronaut_list(coordinates.json())
+    astronaut_list(astronauts.json())
+    
+    parameters = {
+        "lat": -4.2196,
+        "lon": 170.6524
+    }
+    coordinates = requests.get(
+    "http://api.open-notify.org/iss-now.json", params=parameters)
+    astronaut_list(coordinates.json())
+    return (coordinates, astronauts)
+
+
+def world_map(turtle):
+    turtle.Screen()
+    turtle.setup(100)
+    turtle.bgpic(90)
+    turtle.setworldcoordinates(100)
+    
+
+
+    
+    
     
 
 # def main():
-#     astronaut_list()
+#     world_map()
+
 
 # if __name__ == '__main__':
 #     main()
